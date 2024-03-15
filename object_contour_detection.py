46070c04b5d94742f6460090b158b28f61ba3edf
linkgreat <fjw@qq.com>
2024-03-15 13:56:55 +0800

    室内检测#2

--- object_contour_detection.py
+++ object_contour_detection.py
@@ -30,6 +30,7 @@ class OverlapDetectContext:
         self.resize = None
         self.prev_frame = None
         self.prev_time = None
+        self.alarm_time = None
         self.rects = []
         self.prev_img_path = None
         for i in range(16):
@@ -59,6 +60,9 @@ class OverlapDetectContext:
                 return False
             return True
 
+    def get_elapsed_time(self):
+        return time.time() - self.prev_time
+
     def process_contours(self, message, img, img_path):
         params = message.get("params", {})
         self.proc_param(params)
@@ -71,90 +75,6 @@ class OverlapDetectContext:
             self.prev_time = time.time()
             return []
         contour_results = self.calc_overlap_contours_v1(gray_frame, img_path)
-        return contour_results
-
-
-    def calc_overlap_contours(self, gray_frame):
-        current_time = time.time()
-        elapsed_time = current_time - self.prev_time
-        contour_point = []
-        (score, diff) = compare_ssim(self.prev_frame, gray_frame, win_size=91, full=True)
-        diff = (diff * 255).astype("uint8")
-        print("SSIM:{}".format(score))
-        thresh = cv2.threshold(diff, 120, 255, cv2.THRESH_BINARY_INV)[1]
-        contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contour_results = []
-        for c in contours:
-            (x, y, w, h) = cv2.boundingRect(c)
-            if w < 7 or h < 7:
-                continue
-            contour_point.append((x, y, w, h))
-            for j in range(len(contour_point)):
-                cx = contour_point[j][0]
-                cy = contour_point[j][1]
-                cw = contour_point[j][2]
-                ch = contour_point[j][3]
-
-                px = self.rects[j][0]
-                py = self.rects[j][1]
-                pw = self.rects[j][2]
-                ph = self.rects[j][3]
-
-                deltax = abs((cx + cw / 2) - (px + pw / 2))
-                deltay = abs((cy + ch / 2) - (py + ph / 2))
-                overlap_percent = 0.0
-                if deltax < 20 and deltay < 20:
-                    print("出现障碍物")
-
-                    print("elapsed time is ", elapsed_time)
-                    if elapsed_time > 2:
-
-                        crop_img = gray_frame[cy:cy + ch, cx:cx + cw]
-                        prev_crop_img = self.prev_frame[cy:cy + ch, cx:cx + cw]
-                        (crop_score, crop_diff) = compare_ssim(crop_img, prev_crop_img, full=True)
-                        crop_diff = (crop_diff * 255).astype("uint8")
-                        print("crop_SSIM:{}".format(crop_score))
-                        crop_thresh = cv2.threshold(crop_diff, 120, 255, cv2.THRESH_BINARY_INV)[1]
-                        crop_contours, crop_hierarchy = cv2.findContours(crop_thresh.copy(), cv2.RETR_EXTERNAL,
-                                                                         cv2.CHAIN_APPROX_SIMPLE)
-                        for c1 in crop_contours:
-                            if len(c1) >= 3:  # 确保至少有三个点
-
-                                contour_points1 = [(pt[0][0] + cx, pt[0][1] + cy) for pt in c1]  # 提取点
-                                contour_points1.append(contour_points1[0])  # 添加第一个点到末尾以闭合多边形
-                                contour_shape = Polygon(contour_points1)  # 创建多边形
-                                percents = []
-                                for roi_poly in self.include_areas:
-                                    poly = Polygon(roi_poly)
-                                    if poly.area > 0:
-                                        intersection_area = poly.intersection(contour_shape).area
-                                        percent = intersection_area / poly.area
-                                        if percent > 0:
-                                            print('contour overlap percent:{}'.format(percent))
-                                        percents.append(percent)
-                                overlap_percent = max(percent for percent in percents)
-
-                    else:
-                        self.rects[j] = (cx, cy, cw, ch)
-                else:
-                    self.rects[j] = (cx, cy, cw, ch)
-            contour_results.append({
-                "id": int(j),
-                "bBox": {
-                    "x": float(cx / self.resize[0]),
-                    "y": float(cy / self.resize[1]),
-                    "w": float(cw / self.resize[0]),
-                    "h": float(ch / self.resize[1]),
-                },
-                "overlapPercent": overlap_percent,
-                "matched": False,
-            })
-        max_percent = max(
-            (contour["overlapPercent"] if contour["overlapPercent"] is not None else 0 for contour in contour_results),
-            default=0)
-        if len(contour_results) == 0 or max_percent == 0 or elapsed_time >= 15:
-            self.prev_frame = gray_frame
-            self.prev_time = current_time
         return contour_results
 
     def calc_overlap_contours_v1(self, gray_frame, img_path):
@@ -218,7 +138,7 @@ class OverlapDetectContext:
                 else:
                     self.rects[j] = (cx, cy, cw, ch)
 
-        if len(contour_results) == 0 or elapsed_time >= 15:
+        if len(contour_results) == 0 and self.get_elapsed_time() >= 15:
             self.prev_frame = gray_frame
             self.prev_time = current_time
             self.prev_img_path = img_path
@@ -249,9 +169,14 @@ class OccupancyDetector(ClassifierModel):
         ctx = self.get_channel_ctx(task_id)
         alg_result = self.process_image(message, img)
         alg_result["classResults"] = []
+        prev_img_url = ctx.prev_img_path
+        if prev_img_url is None:
+            ctx.process_contours(message, img, img_path)
+            return
         contour_results = ctx.process_contours(message, img, img_path)
         alg_result["contourResults"] = contour_results
         threshold = params.get('overlapThreshold', 0.66)
+        alarm_interval = params.get('alarmInterval', 120)
         for contour in contour_results:
             # 检查每个元素的 overlapPercent 是否大于 0.5
             if contour["overlapPercent"] >= threshold:
@@ -265,23 +190,28 @@ class OccupancyDetector(ClassifierModel):
             (contour["overlapPercent"] if contour["overlapPercent"] is not None else 0 for contour in contour_results),
             default=0)
         alg_result["maxOverlapPercent"] = maxOverlapPercent
-        print("max overlap percent:{}".format(maxOverlapPercent))
+        print("max overlap percent:{} / threshold: {}".format(maxOverlapPercent, threshold))
+        alarm_flag = False
         if maxOverlapPercent >= threshold:
+            alarm_flag = True
             alg_result["alarmFlag"] = True
-            alg_result["prevImgPath"] = ctx.prev_img_path
+            alg_result["prevImgPath"] = prev_img_url
 
         # 这里可以添加图像处理逻辑
         # alg_result = self.process_image(message, img)  # 假设这是你的图像处理函数
+        now = time.time()
         if alg_result is not None and alg_result["maxOverlapPercent"] is not None:
-            # 上传结果
-            target_url = '{}/api/iss/alarm/task/{}/job/{}/result'.format(self.args.addr, task_id, self.name)
-            try:
-                response = requests.post(target_url, json=alg_result)
-                if response.status_code == 200:
-                    print("上传成功")
-                else:
-                    print("上传失败，状态码:", response.status_code)
-                sys.stdout.flush()
-            except Exception as e:
-                print("上传出错:", str(e))
-                sys.stdout.flush()
+            if not alarm_flag or ctx.alarm_time is None or now - ctx.alarm_time >= alarm_interval / 2:
+                # 上传结果
+                target_url = '{}/api/iss/alarm/task/{}/job/{}/result'.format(self.args.addr, task_id, self.name)
+                try:
+                    response = requests.post(target_url, json=alg_result)
+                    if response.status_code == 200:
+                        print("上传成功")
+                    else:
+                        print("上传失败，状态码:", response.status_code)
+                    sys.stdout.flush()
+                    ctx.alarm_time = now
+                except Exception as e:
+                    print("上传出错:", str(e))
+                    sys.stdout.flush()
